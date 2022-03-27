a8d767417bab168d4718554f78bb46b29e064d8e
Ryan Mehri <ryan.mehri1@gmail.com>
2022-03-27 11:35:43 -0700

    feat: implement addition, negation, and subtraction on Tuple

--- src/tuple.rs
+++ src/tuple.rs
@@ -1,3 +1,5 @@
+use std::ops;
+
 /// A tuple using left-hand coordinates where:
 /// * `w = 0.0` is a vector
 /// * `w = 1.0` is a point
@@ -21,6 +23,35 @@ impl Tuple {
 
     pub fn vector(x: f64, y: f64, z: f64) -> Self {
         Self::new(x, y, z, 0.0)
+    }
+}
+
+impl ops::Add for Tuple {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self::new(
+            self.x + other.x,
+            self.y + other.y,
+            self.z + other.z,
+            self.w + other.w,
+        )
+    }
+}
+
+impl ops::Neg for Tuple {
+    type Output = Self;
+
+    fn neg(self) -> Self {
+        Self::new(-self.x, -self.y, -self.z, -self.w)
+    }
+}
+
+impl ops::Sub for Tuple {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        self + -other
     }
 }
 
@@ -62,5 +93,64 @@ mod tests {
         let v2 = Tuple::new(4.3, -4.2, 3.1, 0.0);
 
         assert_eq!(v1, v2);
+    }
+
+    #[test]
+    fn test_tuple_add() {
+        let a1 = Tuple::new(3.0, -2.0, 5.0, 1.0);
+        let a2 = Tuple::new(-2.0, 3.0, 1.0, 0.0);
+
+        let result = a1 + a2;
+
+        assert_eq!(result, Tuple::new(1.0, 1.0, 6.0, 1.0));
+    }
+
+    #[test]
+    fn test_tuple_sub_two_points() {
+        let p1 = Tuple::point(3.0, 2.0, 1.0);
+        let p2 = Tuple::point(5.0, 6.0, 7.0);
+
+        let result = p1 - p2;
+
+        assert_eq!(result, Tuple::vector(-2.0, -4.0, -6.0));
+    }
+
+    #[test]
+    fn test_tuple_sub_vector_from_point() {
+        let p = Tuple::point(3.0, 2.0, 1.0);
+        let v = Tuple::vector(5.0, 6.0, 7.0);
+
+        let result = p - v;
+
+        assert_eq!(result, Tuple::point(-2.0, -4.0, -6.0));
+    }
+
+    #[test]
+    fn test_tuple_sub_two_vectors() {
+        let v1 = Tuple::vector(3.0, 2.0, 1.0);
+        let v2 = Tuple::vector(5.0, 6.0, 7.0);
+
+        let result = v1 - v2;
+
+        assert_eq!(result, Tuple::vector(-2.0, -4.0, -6.0));
+    }
+
+    #[test]
+    fn test_sub_vector_zero_vector() {
+        let zero = Tuple::vector(0.0, 0.0, 0.0);
+        let v = Tuple::vector(1.0, -2.0, 3.0);
+
+        let result = zero - v;
+
+        assert_eq!(result, Tuple::vector(-1.0, 2.0, -3.0));
+    }
+
+    #[test]
+    fn test_tuple_neg() {
+        let a = Tuple::new(1.0, -2.0, 3.0, -4.0);
+
+        let result = -a;
+
+        assert_eq!(result, Tuple::new(-1.0, 2.0, -3.0, 4.0));
     }
 }
